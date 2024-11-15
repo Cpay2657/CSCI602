34353ace1c21b8830bfe14f91483d1c7e8ace1b7
Christopher Payne <Cpayne88820@vt.edu>
2024-11-15 13:25:30 -0500

    Removed old files

--- Project2/test.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.models import Sequential, load_model, Model
-
-model = load_model(r"C:\\Users\\Cpayn\\Desktop\\VSU\\Grad School\\CSCI 602 - Advanced Artificial Intelligence\\Projects\\Project 2\\image_ae\\models\\ae_augmentors")
-
-print(model.history['loss'])
